8ad3760f8ccbcb04b3dfc524588bb9ce1ed259ff
Kevin W <kjoewill@gmail.com>
2018-08-24 15:10:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,11 +20,11 @@ class Song
   
   def initialize(name, artist, genre)
     @@count += 1
+    @@genres << genre
+    @@artists << artist
     @name = name
     @artist = artist
     @genre = genre
-    @@genres << genre
-    @@artists << artist
   end
   
 end
